9f33e66130b090b762b582b9a9965c472fe4c699
James Holman <jameswarringtonholman@gmail.com>
2020-12-20 16:02:54 +1100

    feat: one stubbed route working

--- src/account/adapter/web/account_controller.rs
+++ src/account/adapter/web/account_controller.rs
@@ -1,45 +1,48 @@
-use crate::account::application::port::incoming::GetAccountBalanceQuery;
-use crate::account::domain::*;
-
-use actix_web::{web, HttpRequest, Result};
-use serde::{Deserialize, Serialize};
-
-use std::sync::Arc;
-
-
-#[derive(Deserialize)]
-struct AccountRequest {}
-
-#[derive(Serialize)]
-struct AccountResource {}
-
-struct AccountController {
-getAccountBalanceQuery: Arc<dyn GetAccountBalanceQuery + Sync + Send>,
+// use crate::account::application::port::incoming::GetAccountBalanceQuery;
+#[derive(Debug, Clone, Copy)]
+pub struct AccountController {
+    // getAccountBalanceQuery: Arc<dyn GetAccountBalanceQuery + Sync + Send>,
 // listAccountsQuery: Arc<dyn ListAccountsQuery + Sync + Send>,
 // loadAccountsQuery: Arc<dyn LoadAccountsQuery + Sync + Send>,
 // createAccountUseCase: CreateAccountUseCase,
 }
 
-impl AccountController {
-    // #[get("/accounts")]
+pub mod account_routes {
+    use actix_web::{get, post, web, HttpRequest, HttpResponse, Responder, Result};
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Clone,Copy,Debug,Deserialize,Serialize)]
+    struct AccountRequest {}
+
+    #[derive(Clone,Copy,Debug,Serialize,Deserialize)]
+    pub struct AccountResource {
+        pub accountId: usize,
+    }
+
+    /*
+    #[get("/accounts")]
     pub fn listAccounts() -> Result<Vec<AccountResource>> {
         unimplemented!();
     }
+    */
 
-    // #[get("/accounts/{id}")]
-    pub fn getAccount(req: HttpRequest) -> Result<AccountResource> {
+    #[get("/accounts/{id}")]
+    pub async fn getAccount(req: HttpRequest) -> impl Responder {
+        // Result<web::Json<AccountResource>> {
         let id: usize = req.match_info().query("id").parse().unwrap();
-        unimplemented!();
+        HttpResponse::Ok().json(AccountResource { accountId: id })
     }
 
-    // #[get("/accounts/{id}/balance")]
+    /*
+    #[get("/accounts/{id}/balance")]
     pub fn getAccountBalance(req: HttpRequest) -> Result<usize> {
         let id: usize = req.match_info().query("id").parse().unwrap();
         unimplemented!();
     }
 
-    // #[post("/accounts")]
+    #[post("/accounts")]
     pub fn createAccount(accountReq: web::Json<AccountRequest>) -> Result<AccountResource> {
         unimplemented!();
     }
+    */
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,32 @@
-mod account;
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 
-fn main() {
-    println!("Hello, world!");
+mod account;
+use account::adapter::web::account_controller::{AccountController,account_routes};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+#[derive(Debug,Clone,Copy)]
+struct State {
+    account_controller: AccountController,
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let account_controller = AccountController {};
+    let state = State {
+        account_controller
+    };
+
+    HttpServer::new(move || {
+        App::new()
+            .data(state.clone())
+            .route("/", web::get().to(greet))
+            .service(account_routes::getAccount)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
